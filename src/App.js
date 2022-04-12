234f612539926f1cf973d7abca060fb2fd58db29
erynder-z <stfn.bgr@gmail.com>
2022-04-12 12:01:34 +0200

    Fix imports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './styles/App.css';
 import './styles/Header.css';
 import './styles/Main.css';
 import './styles/Footer.css';
-import { Header } from './components/Header';
-import { Main } from './components/Main';
-import { Footer } from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
