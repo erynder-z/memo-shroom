8537f77177aea14fabe9a3214b2555fc53b02b18
erynder-z <stfn.bgr@gmail.com>
2022-04-12 12:02:44 +0200

    Create initial header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className="navbar ">
+      <div className="header-text">MemoryGame</div>
+    </nav>
+  );
+}
+
+export default Header;
