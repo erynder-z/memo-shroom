04d688667c9b4e2aadefdbe6533adc232969f001
erynder-z <stfn.bgr@gmail.com>
2022-04-12 10:36:18 +0200

    Initialize main component layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import './styles/App.css';
+import './styles/Header.css';
+import './styles/Main.css';
+import './styles/Footer.css';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { Footer } from './components/Footer';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
