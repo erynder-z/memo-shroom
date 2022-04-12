92dab80ed03c781473d21a581c886da00dd1334b
erynder-z <stfn.bgr@gmail.com>
2022-04-12 12:02:33 +0200

    Create initial footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer ">
+      {' '}
+      <div id="myFooter" className="myFooterClass">
+        {'Copyright © Erynder-Z 2022 || '}
+        <a href="https://github.com/erynder-z" target="_blank" rel="noreferrer">
+          My GitHub
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
