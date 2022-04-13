6a5c52384035663e2cac0922576be72301d766f2
erynder-z <stfn.bgr@gmail.com>
2022-04-13 13:04:49 +0200

    Return a card object

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import '../styles/Card.css';
 
-function Card() {
-  return <div className="card">Card</div>;
+function Card(props) {
+  const card = {
+    id: props.id,
+    name: props.name,
+    image: props.image,
+    clicked: props.clicked,
+  };
+  return (
+    <div
+      className="card"
+      id={card.id}
+      name={card.name}
+      clicked={card.clicked}
+      onClick={(e) => {
+        props.cardClick(e, card.id);
+      }}>
+      <img src={card.image} alt="card" />
+    </div>
+  );
 }
 
 export default Card;
