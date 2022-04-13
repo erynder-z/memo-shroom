a6d788cca7a68039e35a231cfaeef5e627ae9cb7
erynder-z <stfn.bgr@gmail.com>
2022-04-13 12:30:50 +0200

    Save images in variable

---
+++ src/components/Deck.js
@@ -0,0 +1,29 @@
+import mushroom1 from '../assets/mushrooms/mush1.png';
+import mushroom2 from '../assets/mushrooms/mush2.png';
+import mushroom3 from '../assets/mushrooms/mush3.png';
+import mushroom4 from '../assets/mushrooms/mush4.png';
+import mushroom5 from '../assets/mushrooms/mush5.png';
+import mushroom6 from '../assets/mushrooms/mush6.png';
+import mushroom7 from '../assets/mushrooms/mush7.png';
+import mushroom8 from '../assets/mushrooms/mush8.png';
+import mushroom9 from '../assets/mushrooms/mush9.png';
+import mushroom10 from '../assets/mushrooms/mush10.png';
+import mushroom11 from '../assets/mushrooms/mush11.png';
+import mushroom12 from '../assets/mushrooms/mush12.png';
+
+const deck = {
+  mushroom1,
+  mushroom2,
+  mushroom3,
+  mushroom4,
+  mushroom5,
+  mushroom6,
+  mushroom7,
+  mushroom8,
+  mushroom9,
+  mushroom10,
+  mushroom11,
+  mushroom12,
+};
+
+export default deck;
