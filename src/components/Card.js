1a0df6d0cc1469046eb7a7db993b1b711e36b616
erynder-z <stfn.bgr@gmail.com>
2022-04-14 18:29:43 +0200

    Destructure and typecheck props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../styles/Card.css';
 
 function Card(props) {
-  const card = {
-    id: props.id,
-    name: props.name,
-    image: props.image,
-    clicked: props.clicked,
-  };
+  const { id, name, image, clicked, cardClick } = props;
   return (
     <div
       className="card"
-      id={card.id}
-      name={card.name}
-      clicked={card.clicked}
+      id={id}
+      name={name}
+      clicked={clicked}
       onClick={(e) => {
-        props.cardClick(e, card.id);
-      }}>
-      <img src={card.image} alt="card" />
+        cardClick(e, id);
+      }}
+      aria-hidden="true">
+      <img src={image} alt="card" />
     </div>
   );
 }
 
 export default Card;
+Card.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  clicked: PropTypes.bool.isRequired,
+  cardClick: PropTypes.func.isRequired,
+};
