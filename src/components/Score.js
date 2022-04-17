31df5fce998d3ed2abe68eabdafe89bbdb322213
erynder-z <stfn.bgr@gmail.com>
2022-04-17 13:13:04 +0200

    Implement highscore

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Score.css';
 
 function Score(props) {
   const { currentScore } = props;
-  return <div className="score">Score: {currentScore}</div>;
+  const [highscore, setHighscore] = useState(0);
+
+  useEffect(() => {
+    if (currentScore > highscore) {
+      setHighscore(() => currentScore);
+    }
+  }, [currentScore]);
+  return (
+    <div className="score-container">
+      <div className="score">Score: {currentScore}</div>
+      <div className="score">Highscore: {highscore}</div>
+    </div>
+  );
 }
 
 export default Score;
