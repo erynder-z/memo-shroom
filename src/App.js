28b2b22c3c01458689655897f0a5cacc0770e8a7
erynder-z <stfn.bgr@gmail.com>
2022-04-17 14:18:32 +0200

    Remove footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './styles/App.css';
 import './styles/Header.css';
 import './styles/Main.css';
-import './styles/Footer.css';
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
-      <Footer />
     </div>
   );
 }
