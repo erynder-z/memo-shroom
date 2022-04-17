48c6f70be80f67ea753658fae174f2d1086157a2
erynder-z <stfn.bgr@gmail.com>
2022-04-17 16:46:55 +0200

    Add about info in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Header() {
+  const [title, setTitle] = useState('Memo-shroom');
+  const showAbout = () => {
+    setTitle(() => (
+      <div className="info">
+        Erynder-Z 2022 ||{' '}
+        <a href="https://github.com/erynder-z" target="_blank" rel="noreferrer">
+          My GitHub
+        </a>{' '}
+        || Mushroom images from{' '}
+        <a href="https://pixabay.com/" target="_blank" rel="noreferrer">
+          Pixabay
+        </a>
+      </div>
+    ));
+  };
+
+  const showTitle = () => {
+    setTitle(() => 'Memo-shroom');
+  };
+
+  useEffect(() => {
+    setTimeout(showTitle, 10000);
+  }, [title]);
+
   return (
     <nav className="navbar ">
-      <div className="header-text">MemoryGame</div>
+      <div className="header-text">{title}</div>
+      <div className="about" onClick={showAbout} onKeyDown={showAbout} role="button" tabIndex="0">
+        ●
+      </div>
     </nav>
   );
 }
